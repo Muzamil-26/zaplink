3e379d7e603f6eaca5bdc5d934b215f5d1073a59
unknown <muzamilkhan.26.10thc@gmail.com>
2024-11-23 21:05:55 +0530

    Fix: Resolved Tailwind CSS issues and optimized code 3

--- nextjs/app/register/page.jsx
+++ nextjs/app/register/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
