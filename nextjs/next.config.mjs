381268375827f8308671dda18003024bc10c7a46
Muzamil-26 <155745476+Muzamil-26@users.noreply.github.com>
2025-10-26 19:02:00 +0530

    Update next.config.js
    This Update is Crucial for Server down But testing a Verp

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
       domains: ['lh3.googleusercontent.com', 'githubusercontent.com','fakestoreapi.com' ,'randomuser.me'], // Allow Google and GitHub profile image URLs
     },
